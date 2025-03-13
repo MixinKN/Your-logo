6aef11a427a93289d1df530b6047be48a696c258
MixinKN <mixin.kn@gmail.com>
2025-03-13 10:59:02 +0300

    completed app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,17 +1,5 @@
-function openMenu() {
-    document.querySelector('.header__row-right').classList.add('header__row-right--is-open')
+import { initNavigation } from "./modules/navigation.js";
 
-}
-
-function closeMenu() {
-    document.querySelector('.header__row-right').classList.remove('header__row-right--is-open')
-
-}
-
-function setHandleMenuCallbacks() {
-    document.querySelector('.header__menu-burger').onclick=openMenu;
-    document.querySelector('.header__menu-close').onclick=closeMenu;
-
-}
-
-setHandleMenuCallbacks()
+document.addEventListener('DOMContentLoaded', () => {
+    initNavigation();
+})
