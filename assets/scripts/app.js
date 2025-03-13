ceac5d31ef10ae8ae4c890b57c676df1982e4b23
MixinKN <mixin.kn@gmail.com>
2025-03-13 10:48:46 +0300

    added app.js

---
+++ assets/scripts/app.js
@@ -0,0 +1,17 @@
+function openMenu() {
+    document.querySelector('.header__row-right').classList.add('header__row-right--is-open')
+
+}
+
+function closeMenu() {
+    document.querySelector('.header__row-right').classList.remove('header__row-right--is-open')
+
+}
+
+function setHandleMenuCallbacks() {
+    document.querySelector('.header__menu-burger').onclick=openMenu;
+    document.querySelector('.header__menu-close').onclick=closeMenu;
+
+}
+
+setHandleMenuCallbacks()
